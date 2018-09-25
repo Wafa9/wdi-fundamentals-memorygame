bca227cfd1484ca8f48632cd9eada261b3d5906a
wafa turaifi <wafa.a.t14@gmail.com>
2018-09-26 01:43:25 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,25 +2,39 @@
 
 
 
-   var cards=["queen","queen","king","king"];
+   var cards=["queen","king"];
 
    var cardsInPlay=[];
 
-    var card1=cards[0];
-    cardsInPlay.push('card1'); 
-    console.log("User flipped queen");
+    function checkForMatch()
+    {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+     console.log("Sorry, try again.");
+}
 
-    var card2= cards[1];
-    cardsInPlay.push('card2'); 
-     console.log("User flipped king");
-     
-     
-     if (cardsInPlay.length ===2) {
+    }
 
-     	if(lcardsInPlay[0]===cardsInPlay[1]) {
-     		alert("You found a match!"); 
-     	}
-     	else{
-     		alert("Sorry, try again.!");
-     	}
+     function flipCard(cardId){
+        
+     var cardsInPlay=[];
+     console.log("User flipped " + cards);
+     cardsInPlay.push(cards[cardId]);
+     
+     if (cardsInPlay.length ===  2) {
+
+        if(lcardsInPlay[0]  ===  cardsInPlay[1]) {
+            alert("You found a match!"); 
+        }
+        else{
+            alert("Sorry, try again.!");
+        }
      }
+     
+     }
+
+     flipCard.call (cards[0]);
+     flipCard.call (cards[2]);
+
+
